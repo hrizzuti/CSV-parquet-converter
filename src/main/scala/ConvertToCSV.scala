9b1d817e0c1ab0d014de2efef7da804541d20fdb
rizzutih <h.rizzuti@elsevier.com>
2022-08-02 15:30:38 +0100

    EPIC-670: create Journal Family csv file

--- src/main/scala/ConvertToCSV.scala
+++ src/main/scala/ConvertToCSV.scala
@@ -2,18 +2,22 @@ import org.apache.spark.sql.{SaveMode, SparkSession}
 
 object ConvertToCSV extends App {
 
+
   val spark: SparkSession = SparkSession.builder()
     .master("local[1]")
     .appName("Converter")
     .getOrCreate()
 
+  val filename = "part-00000-b7ab4984-4757-480a-81f9-a2b98bf645bf-c000.snappy"
 
-  val df = spark.read.format("parquet")
-    .load("src/main/filesInput/metric_value.parquet")
+  val df = spark.read
+    .format("parquet")
+    .load(s"src/main/filesInput/$filename.parquet")
 
-
-  df.write.mode(SaveMode.Overwrite).format("csv").csv("src/main/filesOutput/")
-
-  spark.close()
+  df.write
+    .mode(SaveMode.Append)
+    .option("mapreduce.fileoutputcommitter.marksuccessfuljobs", "false")
+    .option("header", "true")
+    .csv("src/main/filesOutput/")
 
 }
--- src/main/scala/ConvertToParquet.scala
+++ src/main/scala/ConvertToParquet.scala
@@ -7,12 +7,11 @@ object ConvertToParquet extends App {
     .appName("Converter")
     .getOrCreate()
 
+  val filename = "part-00000-24214a2a-f8d2-491b-ab23-cce86370fe8d-c000"
 
-  val df = spark.read.options(Map("inferSchema"->"true","delimiter"->",","header"->"true"))
-    .csv("src/main/filesInput/division.csv")
-
-
-  df.write.mode(SaveMode.Overwrite).option("compression","none").parquet("src/main/filesOutput/")
+  val df = spark.read
+    .options(Map("inferSchema"->"true","delimiter"->",","header"->"true"))
+    .csv(s"src/main/filesInput/$filename.csv")
 
   spark.close()
 
